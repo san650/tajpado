b58a1e906f3303ea785bf1e9cf20ec3a32e1fca4
Santiago Ferreira <san650@gmail.com>
2016-02-05 23:13:57 -0300

    Move script to its own model

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
+import Script from 'tajpado/models/script';
 
 export default Ember.Route.extend({
   model() {
-    return "A windmill is a mill that converts the energy of wind into rotational energy by means of vanes called sails or blades. Traditional windmills were often used to mill grain, pump water, or both. Most modern windmills take the form of wind turbines used to generate electricity, or windpumps used to pump water, either for land drainage or to extract groundwater.";
+    return Script.find(0);
   },
 
   actions: {
